5cc8683876ca3b868d020e7ed53873c89c295125
larzraz <lars5030@edu.eal.dk>
2019-09-24 10:37:27 +0200

    Removed views from mainviewtabbed items

--- SportCCAPItesting/SportCCAPItesting/MainView.xaml.cs
+++ SportCCAPItesting/SportCCAPItesting/MainView.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using SportCCAPItesting.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,11 +11,8 @@ namespace SportCCAPItesting
         public MainView()
         {
             InitializeComponent();
-            this.Children.Add(new Sports());
-            this.Children.Add(new CountryList());
-            this.Children.Add(new Matches());
             this.Children.Add(new TodaysMatches());
-         
+            this.Children.Add(new CountryList());
         }
     }
 }
